f086a24f2a09eda749bbe7dc75ad1827db42f3fd
Karthik Udyawar <karthikajitudy@gmail.com>
2022-07-10 00:59:33 +0530

    Update Data.routes.js
    ### Changed
    
    - Added download router.

--- server/routers/Data.routes.js
+++ server/routers/Data.routes.js
@@ -1,8 +1,10 @@
 import express from "express";
+import { getData } from "../controllers/getData.js";
 import { postData } from "../controllers/postData.js";
 
 const router = express.Router();
 
+router.route("/download").get(getData);
 router.route("/postData").post(postData);
 
 export default router;
